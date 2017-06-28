1600a8c322897acf4f0095b6f3ef9f5ad3b0dcf3
kibigo! <marrus-sh@users.noreply.github.com>
2017-06-27 17:01:46 -0700

    Put images behind CWs
    Many improvements to images in collapsed toots
    
    - Now works on detailed and static pages
    - Fixed bug with nested CW / Sensitive Media
    - Now apparent which toots contain media

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -275,6 +275,7 @@ class Status extends ImmutablePureComponent {
     let statusAvatar, prepend, rebloggedByText;
 
     const { intl, hidden, featured, otherAccounts, unread, showThread } = this.props;
+    let mediaIcon = null;
 
     let { status, account, ...other } = this.props;
 
@@ -391,6 +392,7 @@ class Status extends ImmutablePureComponent {
             )}
           </Bundle>
         );
+        mediaIcon = 'video-camera';
       } else {
         media = (
           <Bundle fetchComponent={MediaGallery} loading={this.renderLoadingMediaGallery}>
@@ -408,6 +410,7 @@ class Status extends ImmutablePureComponent {
             )}
           </Bundle>
         );
+        mediaIcon = 'picture-o';
       }
     } else if (status.get('spoiler_text').length === 0 && status.get('card')) {
       media = (
@@ -448,9 +451,11 @@ class Status extends ImmutablePureComponent {
               </a>
             </div>
 
-            <StatusContent status={status} onClick={this.handleClick} expanded={!status.get('hidden')} onExpandedToggle={this.handleExpandedToggle} collapsable />
+            <StatusContent mediaIcon={mediaIcon} status={status} onClick={this.handleClick} expanded={!status.get('hidden')} onExpandedToggle={this.handleExpandedToggle} collapsable>
 
-            {media}
+                {media}
+
+            </StatusContent>
 
             {showThread && status.get('in_reply_to_id') && status.get('in_reply_to_account_id') === status.getIn(['account', 'id']) && (
               <button className='status__content__read-more-button' onClick={this.handleClick}>
