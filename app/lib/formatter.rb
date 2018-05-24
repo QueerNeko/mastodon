0b88d44cbe28bbc63a6b0235482561945406a303
QueerNeko <>
2018-05-23 22:03:00 -0300

    [app] formatter: Adding Instagram mention link

--- app/lib/formatter.rb
+++ app/lib/formatter.rb
@@ -278,6 +278,8 @@ class Formatter
       return link_to_github(username)
 	when 'orientando.org'
 	  return link_to_orientando(username)
+	when 'instagram.com'
+	  return link_to_instagram(username)
     else
       return link_to_account(acct) unless linkable_accounts
     end
@@ -308,6 +310,10 @@ class Formatter
   
   def link_to_orientando(username)
     "<span class=\"h-card\"><a href=\"https://orientando.org/membres/#{username}\" target=\"blank\" rel=\"noopener noreferrer\" class=\"u-url mention\">@<span>#{username}@orientando.org</span></a></span>"
+  end
+  
+  def link_to_instagram(username)
+    "<span class=\"h-card\"><a href=\"https://www.instagram.com/#{username}\" target=\"blank\" rel=\"noopener noreferrer\" class=\"u-url mention\">@<span>#{username}@instagram.com</span></a></span>"
   end
 
   def link_to_account(acct)
