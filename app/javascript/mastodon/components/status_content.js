8a5f45a4aa12d959e68d953430d0a8ec4ecd235b
QueerNeko <QueerNeko@users.noreply.github.com>
2018-08-26 18:03:23 -0300

    Fixing image inside cw bugs

--- app/javascript/mastodon/components/status_content.js
+++ app/javascript/mastodon/components/status_content.js
@@ -175,7 +175,7 @@ export default class StatusContent extends React.PureComponent {
   render () {
     const { status, children, mediaIcon } = this.props;
 
-    if (status.get('content').length === 0) {
+    if (status.get('content').length === 0 && children === null) {
       return null;
     }
 
@@ -229,8 +229,7 @@ export default class StatusContent extends React.PureComponent {
 
           {mentionsPlaceholder}
 
-          {!hidden &&
-          <div tabIndex={!hidden ? 0 : null} className={`status__content__text ${!hidden ? 'status__content__text--visible' : ''}`}>
+          <div tabIndex={!hidden ? 0 : null} className={`status__content__spoiler ${!hidden ? 'status__content__spoiler--visible' : ''}`}>
 
             <div
               style={directionStyle}
@@ -238,11 +237,9 @@ export default class StatusContent extends React.PureComponent {
               onMouseUp={this.handleMouseUp}
               dangerouslySetInnerHTML={content}
             />
-
             {children}
             {!hidden && !!status.get('poll') && <PollContainer pollId={status.get('poll')} />}
           </div>
-          }
         </div>
       );
     } else if (this.props.onClick) {
