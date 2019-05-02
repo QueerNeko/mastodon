541bc87de0657a58071bb5cdb32c9a25444de4a1
QueerNeko <QueerNeko@users.noreply.github.com>
2019-05-02 20:11:15 -0300

    Increasing account fields size to 6, increasing bio size to 800

--- app/models/account.rb
+++ app/models/account.rb
@@ -75,8 +75,8 @@ class Account < ApplicationRecord
   validates_with UniqueUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates :display_name, length: { maximum: 40 }, if: -> { local? && will_save_change_to_display_name? }
-  validates :note, note_length: { maximum: 500 }, if: -> { local? && will_save_change_to_note? }
-  validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
+  validates :note, note_length: { maximum: 800 }, if: -> { local? && will_save_change_to_note? }
+  validates :fields, length: { maximum: 6 }, if: -> { local? && will_save_change_to_fields? }
 
   scope :remote, -> { where.not(domain: nil) }
   scope :local, -> { where(domain: nil) }
@@ -262,7 +262,7 @@ class Account < ApplicationRecord
     self[:fields] = fields
   end
 
-  DEFAULT_FIELDS_SIZE = 4
+  DEFAULT_FIELDS_SIZE = 6
 
   def build_fields
     return if fields.size >= DEFAULT_FIELDS_SIZE
