e86a40138cca567839ff4c5b0a0c65d61cc2ed4b
QueerNeko <QueerNeko@users.noreply.github.com>
2019-06-04 22:57:35 -0300

    Fixing video height not being set after CW is open

--- app/javascript/mastodon/features/video/index.js
+++ app/javascript/mastodon/features/video/index.js
@@ -107,6 +107,7 @@ class Video extends React.PureComponent {
     intl: PropTypes.object.isRequired,
     blurhash: PropTypes.string,
     link: PropTypes.node,
+    parentHidden: PropTypes.bool,
   };
 
   state = {
@@ -134,7 +135,7 @@ class Video extends React.PureComponent {
   setPlayerRef = c => {
     this.player = c;
 
-    if (c) {
+    if (c && c.offsetWidth != 0) {
       if (this.props.cacheWidth) this.props.cacheWidth(this.player.offsetWidth);
       this.setState({
         containerWidth: c.offsetWidth,
@@ -289,8 +290,14 @@ class Video extends React.PureComponent {
   }
 
   componentDidUpdate (prevProps, prevState) {
-    if (prevState.revealed && !this.state.revealed && this.video) {
+    if (((prevState.revealed && !this.state.revealed) || (!prevProps.parentHidden && this.props.parentHidden)) && this.video) {
       this.video.pause();
+    }
+    if (!this.props.parentHidden && this.player && this.player.offsetWidth != 0 && this.player.offsetWidth != this.state.containerWidth) {
+      if (this.props.cacheWidth) this.props.cacheWidth(this.player.offsetWidth);
+      this.setState({
+        containerWidth: this.player.offsetWidth,
+      });
     }
     if (prevProps.blurhash !== this.props.blurhash && this.props.blurhash) {
       this._decode();
