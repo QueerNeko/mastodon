8d41929830d65816f39cc4cf135b21642159e32e
QueerNeko <QueerNeko@users.noreply.github.com>
2019-06-16 19:11:07 -0300

    Fixing Media Gallery size inside Web UI

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -376,6 +376,7 @@ class Status extends ImmutablePureComponent {
                 defaultWidth={this.props.cachedMediaWidth}
                 visible={this.state.showMedia}
                 onToggleVisibility={this.handleToggleMediaVisibility}
+                parentHidden={status.get('hidden')}
               />
             )}
           </Bundle>
